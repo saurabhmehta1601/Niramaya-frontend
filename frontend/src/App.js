0e938e2d6e059f425e74faffc4943d2782bcb89c
saurabh <saurabhmehta1601@gmail.com>
2021-10-24 10:58:18 +0530

    dashboard layout done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import HomePage from "./pages/Home"
 import LoginPage from "./pages/Login"
 import RegisterPage from "./pages/Register"
+import ProfilePage from "./pages/Profile"
+
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+
 import {Switch,Route,BrowserRouter} from "react-router-dom"
 
 function App() {
@@ -13,6 +16,8 @@ function App() {
         <Route exact path="/" component={HomePage} ></Route>
         <Route path="/login" component={LoginPage} ></Route>
         <Route path="/register" component={RegisterPage}></Route>
+        <Route path="/register" component={RegisterPage}></Route>
+        <Route path="/profile/:id" component={ProfilePage}></Route>
       </Switch>
     <Footer/>
     </BrowserRouter>
