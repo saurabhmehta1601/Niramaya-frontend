1eade151a83c8b7cb430c72ba374d2074957dc9c
Saurabh <57309205+saurabhmehta1601@users.noreply.github.com>
2021-11-02 18:09:50 +0530

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -6,15 +6,14 @@ fetch(url)
         const users = data.map(d => {
             return {
                 name:d.author.login,
-                img_url : d.author.avatar_url,
-                profile:d.author.url
+                img_url : d.author.avatar_url                
             }
         })
         users.forEach(user =>{
             const contributor = document.createElement('div')
             contributor.classList.add("contributor")  
             contributor.innerHTML = `
-                <a href=${user.profile}>
+                <a href=https://www.github.com/${user.name}>
                     <img src=${user.img_url} alt=${user.name}/> 
                 </a>
                <h3>${user.name}</h3>
